ef681c8496b81fc7d426671414a158df25246374
jdkimber <joshua.kimber@wti-solutions.com>
2020-01-21 13:47:36 -0500

    adds navigation, and recipes

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import { Recipe } from '../recipe.model';
 
 @Component({
@@ -7,14 +7,19 @@ import { Recipe } from '../recipe.model';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
   recipes: Recipe[] = [
     new Recipe('A Test Recipe', 'This is simply a test', 'https://www.edicionrural.com/wp-content/uploads/2019/09/pescado.jpg'),
-    new Recipe('A Test Recipe', 'This is simply a test', 'https://www.edicionrural.com/wp-content/uploads/2019/09/pescado.jpg')
+    new Recipe('Another Test Recipe', 'This is simply a test', 'https://www.edicionrural.com/wp-content/uploads/2019/09/pescado.jpg')
   ];
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onRecipeSelected(recipe: Recipe) {
+    this.recipeWasSelected.emit(recipe);
   }
 
 }
